0e9c0db9ce5b3df03375f02535459fa591d64a1d
root <root@c4ac66700a1c.mylabserver.com>
2020-08-22 03:06:25 +0000

    Updated the config files based on OS Family

---
+++ manifests/config.pp
@@ -0,0 +1,12 @@
+# @summary 
+# Includes configuration file based on Os Family
+class apache::config {
+  file { 'apache_config':
+    ensure => "$apache::config_ensure",
+    path   => "$apache::config_path",
+    source => "puppet:///modules/apache/${osfamily}.conf",
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+  }
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -7,6 +7,12 @@
 class apache (
   String $install_name,
   String $install_ensure,
+  String $config_ensure,
+  String $config_path,
 ) { 
-  include apache::install
+  contain apache::install
+  contain apache::config
+
+  Class['::apache::install']
+  -> Class['::apache::config']
 }
